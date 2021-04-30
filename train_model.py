f2b68c40be9758e54339ff64c211285c34f5ec61
gtoutin <gctoutin@gmail.com>
2021-04-29 23:00:57 -0500

    I hope this works

--- train_model.py
+++ train_model.py
@@ -9,6 +9,8 @@ import torchvision
 import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
+import os
 
 # define what our classifier network will look like. necessary for both loading our model again and starting a model for the first time.
 class Net(nn.Module):
@@ -33,13 +35,57 @@ class Net(nn.Module):
 
 batch_size = 4  # how many images at a time
 
+# define a CNN. it's an instance of the Net class defined above.
+net = Net()
 
 # load a model if it exists
+if os.path.isfile(PATH):
+    print('Loading previous model...')
+    net.load_state_dict(torch.load(PATH))
+    print('Model loaded.')
 
 
+print('Loading dataset...')
 # load the datasets using ImageFolder
 trainset = torchvision.datasets.ImageFolder(root=TRAINDIR)
 # get a dataloader to easily navigate thru the dataset for training
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                           shuffle=True, num_workers=2)
+print('Dataset loaded.')
 
+# define loss function and optimizer
+criterion = nn.CrossEntropyLoss()  # good criterion for classifiers
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)  # stochastic gradient descent
+
+print('Starting training...')
+# train the net ---------------------------------------------
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training!')
+
+
+print('Saving progress...')
+# save progress
+torch.save(net.state_dict(), PATH)
+print('Model saved.')
